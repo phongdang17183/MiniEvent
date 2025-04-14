80dbc704e6cee85f16e3c91c8b43501603fe3c95
Brook Vita <bachchinhtran@gmail.com>
2025-04-14 12:11:55 +0700

    feat(aop): add log before call controller

---
+++ src/main/java/com/example/MiniEvent/aop/LoggingAspect.java
@@ -0,0 +1,23 @@
+package com.example.MiniEvent.aop;
+
+import lombok.extern.slf4j.Slf4j;
+import org.aspectj.lang.JoinPoint;
+import org.aspectj.lang.annotation.Aspect;
+import org.aspectj.lang.annotation.Before;
+import org.springframework.stereotype.Component;
+
+@Aspect
+@Slf4j
+@Component
+public class LoggingAspect {
+
+    @Before("execution(* com.example.MiniEvent.service..*(..))")
+    public void logBeforeServiceCall(JoinPoint joinPoint) {
+        log.info("Calling service method {}", joinPoint.getSignature().getName());
+    }
+
+    @Before("execution(* com.example.MiniEvent.controller..*(..))")
+    public void logBeforeControllerCall(JoinPoint joinPoint) {
+        log.info("Calling controller method {}", joinPoint.getSignature().getName());
+    }
+}
