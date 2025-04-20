6d0ee09a0f389f045a1d16e1444fe988228d1160
Brook Vita <bachchinhtran@gmail.com>
2025-04-21 00:03:35 +0700

    feat(test): add update event test

--- src/test/java/com/example/MiniEvent/usecase/EventUseCaseTest.java
+++ src/test/java/com/example/MiniEvent/usecase/EventUseCaseTest.java
@@ -1,29 +1,40 @@
 package com.example.MiniEvent.usecase;
 
+import com.example.MiniEvent.adapter.web.dto.mapper.EventMapper;
+import com.example.MiniEvent.adapter.web.dto.request.UpdateEventRequest;
+import com.example.MiniEvent.adapter.web.dto.request.UpdateEventRequestDTO;
 import com.example.MiniEvent.model.entity.Event;
 import com.example.MiniEvent.adapter.repository.EventRepository;
+import com.example.MiniEvent.service.inteface.AuthService;
 import com.example.MiniEvent.usecase.impl.CreateEventUseCaseImpl;
 import com.example.MiniEvent.service.inteface.ImageStorageService;
 import com.example.MiniEvent.adapter.web.dto.request.CreateEventRequest;
+import com.example.MiniEvent.usecase.impl.UpdateEventUseCaseImpl;
 import com.google.cloud.Timestamp;
 import com.google.cloud.firestore.GeoPoint;
+import com.google.firebase.auth.FirebaseToken;
 import org.junit.jupiter.api.BeforeEach;
 import org.junit.jupiter.api.Test;
 import org.junit.jupiter.api.extension.ExtendWith;
 import org.mockito.InjectMocks;
 import org.mockito.Mock;
 import org.mockito.junit.jupiter.MockitoExtension;
+import org.mockito.junit.jupiter.MockitoSettings;
+import org.mockito.quality.Strictness;
 import org.springframework.mock.web.MockMultipartFile;
 import org.springframework.security.core.Authentication;
 import org.springframework.security.core.context.SecurityContext;
 import org.springframework.security.core.context.SecurityContextHolder;
 import org.springframework.web.multipart.MultipartFile;
 
+import java.util.Optional;
+
 import static org.junit.jupiter.api.Assertions.*;
 import static org.mockito.ArgumentMatchers.any;
 import static org.mockito.Mockito.*;
 
 @ExtendWith(MockitoExtension.class)
+@MockitoSettings(strictness = Strictness.LENIENT)
 public class EventUseCaseTest {
     @Mock
     private EventRepository eventRepository;
@@ -31,8 +42,17 @@ public class EventUseCaseTest {
     @Mock
     private ImageStorageService imageStorageService;
 
+    @Mock
+    private EventMapper eventMapper;
+
+    @Mock
+    private AuthService authService;
+
     @InjectMocks
-    private CreateEventUseCaseImpl eventUseCaseImpl;
+    private CreateEventUseCaseImpl createEventUseCase;
+
+    @InjectMocks
+    private UpdateEventUseCaseImpl updateEventUseCase;
 
     private CreateEventRequest createEventRequest;
     private MultipartFile imageFile;
@@ -71,11 +91,11 @@ public class EventUseCaseTest {
     }
 
     @Test
-    void createEvent_success_withoutImage() throws Exception {
+    void createEvent_success_withoutImage() {
 
         when(eventRepository.save(any(Event.class))).thenReturn(event);
 
-        Event result = eventUseCaseImpl.createEvent(createEventRequest, null);
+        Event result = createEventUseCase.createEvent(createEventRequest, null);
 
         assertNotNull(result);
         assertEquals("Test Event", result.getName());
@@ -94,19 +114,58 @@ public class EventUseCaseTest {
     }
 
     @Test
-    void createEvent_withImage_shouldUploadAndCreateEvent() throws Exception {
+    void createEvent_withImage_shouldUploadAndCreateEvent() {
 
         String uploadedImageUrl = "https://cloudinary.com/my-upload.jpg";
         when(imageStorageService.uploadImage(any())).thenReturn(uploadedImageUrl);
         when(eventRepository.save(any())).thenAnswer(i -> i.getArgument(0));
 
-        Event result = eventUseCaseImpl.createEvent(createEventRequest, imageFile);
+        Event result = createEventUseCase.createEvent(createEventRequest, imageFile);
 
         assertNotNull(result);
         assertEquals(uploadedImageUrl, result.getImage());
 
         verify(imageStorageService).uploadImage(imageFile);
         verify(eventRepository).save(any());
+    }
+
+    @Test
+    void updateEvent_success_withoutImage() {
+
+        // Set up
+        String idToken = "abc";
+        String uid = "uid123";
+        String eventId = "event123";
+        GeoPoint updateLocation = new GeoPoint(11.7769,13.7009);
+        Timestamp updateDate = Timestamp.ofTimeMicroseconds(1745147570000000L);
+        UpdateEventRequest updateEventRequest = new UpdateEventRequest("update event", updateLocation, "A update test event", updateDate, true, true, 30);
+        UpdateEventRequestDTO updateEventRequestDTO = new UpdateEventRequestDTO(idToken, eventId, updateEventRequest, null);
+        FirebaseToken firebaseToken = mock(FirebaseToken.class);
+
+
+        when(firebaseToken.getUid()).thenReturn(uid);
+        when(authService.verifyToken(idToken)).thenReturn(firebaseToken);
+        when(eventRepository.findById("event123")).thenReturn(Optional.ofNullable(event));
+        doAnswer(invocation -> {
+            event.setName(updateEventRequest.getName());
+            event.setDescription(updateEventRequest.getDescription());
+            event.setDate(updateEventRequest.getDate());
+            return null;
+        }).when(eventMapper).updateEventFromRequest(updateEventRequest, event);
+        when(eventRepository.save(any(Event.class))).thenReturn(event);
+
+        // Act
+        Event updatedEvent  = updateEventUseCase.updateEvent(updateEventRequestDTO);
+
+        // Assert
+        assertEquals("update event", updatedEvent.getName());
+        assertEquals("A update test event", updatedEvent.getDescription());
+
+        verify(authService).verifyToken(idToken);
+        verify(eventRepository).findById(eventId);
+        verify(eventRepository).save(event);
+        verify(imageStorageService, never()).uploadImage(any());
+        verify(eventMapper).updateEventFromRequest(updateEventRequest, event);
     }
 
 }
